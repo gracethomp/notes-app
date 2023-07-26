5acee4cee6ec921fc234e8afbfad4ac30936b15e
Olena Babii <gracetomlinson26@gmail.com>
2023-07-27 00:21:48 +0300

    archive nodes, separate file for icons, dynamic category icon

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
+import { actions, task, idea, randomThoughts } from "./icons.js";
+
 const categories = [
   {
     name: "Task",
     active: 3,
     archived: 0,
+    icon: task,
   },
   {
     name: "Random Thought",
     active: 2,
     archived: 0,
+    icon: randomThoughts,
   },
   {
     name: "Idea",
     active: 2,
     archived: 0,
+    icon: idea,
   },
 ];
 
@@ -23,6 +28,7 @@ let notesData = [
     noteCategory: "Task",
     noteContent: "Tomatoes, Bread",
     datesMentioned: [],
+    archived: false,
   },
   {
     name: "The theory of evolution",
@@ -31,6 +37,7 @@ let notesData = [
     noteContent:
       "Life's journey is an unpredictable dance, where the steps we take shape the music we leave behind.",
     datesMentioned: [],
+    archived: false,
   },
   {
     name: "New feature",
@@ -38,6 +45,7 @@ let notesData = [
     noteCategory: "Idea",
     noteContent: "Implement new feature (3/5/2021, 5/5/2021)",
     datesMentioned: ["3/5/2021", "5/5/2021"],
+    archived: false,
   },
   {
     name: "Sweet dream",
@@ -45,6 +53,7 @@ let notesData = [
     noteCategory: "Random Thought",
     noteContent: "Had an interesting dream last night",
     datesMentioned: [],
+    archived: false,
   },
   {
     name: "Birthday gift",
@@ -53,6 +62,7 @@ let notesData = [
     noteContent:
       "Grace has a birthday on 17/05/2021. Don't forget to buy a gift.",
     datesMentioned: ["17/05/2021"],
+    archived: false,
   },
   {
     name: "Trip",
@@ -60,6 +70,7 @@ let notesData = [
     noteCategory: "Task",
     noteContent: "Plan a weekend trip",
     datesMentioned: [],
+    archived: false,
   },
   {
     name: "Pet-project",
@@ -67,45 +78,32 @@ let notesData = [
     noteCategory: "Idea",
     noteContent: "Idea for a new project: Create a recipe sharing app",
     datesMentioned: [],
+    archived: false,
   },
 ];
 
+let showArchivedNotes = false;
+
+function selectCategoryIcon(category) {
+  return categories.find((item) => item.name === category).icon;
+}
+
 function updateNotesTable() {
   const tableBody = document.querySelector(".note-list");
+  tableBody.innerHTML = "";
   notesData.forEach((note, index) => {
-    if (!note.archived) {
+    if (showArchivedNotes == note.archived) {
       const row = document.createElement("tr");
-      row.innerHTML = `
-                <td>
-                    <div class='category-icon'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list-task category-icon-img" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M2 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5V3a.5.5 0 0 0-.5-.5H2zM3 3H2v1h1V3z"/>
-                            <path d="M5 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM5.5 7a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 4a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9z"/>
-                            <path fill-rule="evenodd" d="M1.5 7a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5V7zM2 7h1v1H2V7zm0 3.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5H2zm1 .5H2v1h1v-1z"/>
-                        </svg>
-                    </div>
-                </td>
+      row.innerHTML =
+        "<td>" +
+        selectCategoryIcon(note.noteCategory) +
+        `</td>
                 <td>${note.name}</td>
                 <td>${note.timeOfCreation}</td>
                 <td>${note.noteCategory}</td>
                 <td>${note.noteContent}</td>
-                <td>${note.datesMentioned.join(", ")}</td>
-                <td>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-archive" viewBox="0 0 16 16">
-                        <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
-                    </svg>
-                </td>
-                <td>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                    </svg>
-                </td>
-                <td>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
-                    </svg>
-                </td>
-            `;
+                <td>${note.datesMentioned.join(", ")}</td>` +
+        actions(index);
       tableBody.appendChild(row);
     }
   });
@@ -117,13 +115,7 @@ function updateCategoriesTable() {
     const row = document.createElement("tr");
     row.innerHTML = `
                 <td class="category-cell">
-                    <div class='category-icon'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list-task category-icon-img" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M2 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5V3a.5.5 0 0 0-.5-.5H2zM3 3H2v1h1V3z"/>
-                            <path d="M5 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM5.5 7a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 4a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9z"/>
-                            <path fill-rule="evenodd" d="M1.5 7a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5V7zM2 7h1v1H2V7zm0 3.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5H2zm1 .5H2v1h1v-1z"/>
-                        </svg>
-                    </div>
+                    ${category.icon}
                 </td>
                 <td>${category.name}</td>
                 <td>${category.active}</td>
@@ -132,6 +124,37 @@ function updateCategoriesTable() {
     tableBody.appendChild(row);
   });
 }
+
+const activeNotesSection = document.querySelector(".active-notes-option>a");
+const archiveNotesSection = document.querySelector(".archived-notes-option>a");
+
+function changeHeading(heading) {
+  const h2 = document.querySelector(".notes-title");
+  h2.innerText = heading;
+}
+
+function selectActiveNotes() {
+  showArchivedNotes = false;
+  activeNotesSection.classList.remove("link-body-emphasis");
+  activeNotesSection.classList.add("link-secondary");
+  archiveNotesSection.classList.remove("link-secondary");
+  archiveNotesSection.classList.add("link-body-emphasis");
+  changeHeading("My Notes");
+  updateNotesTable();
+}
+
+function selectArchivedNotes() {
+  showArchivedNotes = true;
+  archiveNotesSection.classList.add("link-secondary");
+  archiveNotesSection.classList.remove("link-body-emphasis");
+  activeNotesSection.classList.add("link-body-emphasis");
+  activeNotesSection.classList.remove("link-secondary");
+  changeHeading("Archive");
+  updateNotesTable();
+}
+
+activeNotesSection.addEventListener("click", selectActiveNotes);
+archiveNotesSection.addEventListener("click", selectArchivedNotes);
 
 updateNotesTable();
 updateCategoriesTable();
